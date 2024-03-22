c2be426a9439965e5ec1dfbf8ee36c4a0530c3d3
alexkjzz <Alex.lam@orange.fr>
2024-03-22 13:46:00 +0100

    oui

--- script.js
+++ script.js
@@ -45,33 +45,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-    
-// Fonction pour ouvrir/fermer le menu
-// Sélection des éléments du menu
-const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu-items .item');
-const menuToggle = document.querySelector('.menu-toggle');
-
-// Fonction pour ouvrir/fermer le menu
-function toggleMenu() {
-  menuItems.forEach(item => {
-    item.classList.toggle('active');
-  });
-  menuToggle.classList.toggle('active');
-}
-
-// Ajout d'un gestionnaire d'événement au bouton de menu pour ouvrir/fermer le menu
-menuToggle.addEventListener('click', toggleMenu);
-
-// Fonction pour fermer le menu lorsqu'un clic est effectué à l'extérieur
-function closeMenu(event) {
-  if (!menu.contains(event.target)) {
-    menuItems.forEach(item => {
-      item.classList.remove('active');
-    });
-    menuToggle.classList.remove('active');
-  }
-}
-
-// Ajout d'un gestionnaire d'événement pour le clic sur le corps
-document.addEventListener('click', closeMenu);
